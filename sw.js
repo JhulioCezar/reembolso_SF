66e046ca1ca3dcec19c49a5ba298e2be904c3160
JhulioCezar <131815120+JhulioCezar@users.noreply.github.com>
2025-10-12 19:18:31 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'reembolso-sf-cache-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+  'https://i.imgur.com/dvzRyus.png'
+];
+
+// Evento de Instalação: Salva os arquivos principais em cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aberto');
+        return cache.addAll(URLS_TO_CACHE);
+      })
+  );
+});
+
+// Evento de Fetch: Intercepta as requisições
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Se encontrar no cache, retorna a resposta do cache
+        if (response) {
+          return response;
+        }
+        // Se não, busca na rede
+        return fetch(event.request);
+      })
+  );
+});
+
+// Evento de Ativação: Limpa caches antigos
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
